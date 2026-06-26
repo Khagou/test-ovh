0ef764d1e38456afefa6e793d7fc4e68012c8c40
VERDOM <maxence.verdom@cgi.com>
2026-06-26 11:39:40 +0200

    try fix provider fmt et passage des secrets direct dans secret github

--- provider.tf
+++ provider.tf
@@ -7,17 +7,13 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "test-tfstate"   # nom de ton bucket OVH Object Storage
+    bucket = "test-tfstate"
     key    = "ovh/terraform.tfstate"
     region = "eu-west-par"
 
     endpoints = {
       s3 = "https://s3.eu-west-par.io.cloud.ovh.net"
     }
-
-    # Credentials S3 de l'OpenStack user créé pour le bucket
-    # Passés via env vars AWS_ACCESS_KEY_ID / AWS_SECRET_ACCESS_KEY
-    # (ou -backend-config à l'init)
 
     skip_credentials_validation = true
     skip_region_validation      = true
@@ -28,6 +24,6 @@ terraform {
 
 provider "ovh" {
   endpoint      = "ovh-eu"
-  client_id     = "EU.12e8aed2e6667fcf"
-  client_secret = "064b3412188866a5ff2f58acefd48044Q"
+  client_id     = var.ovh_client_id
+  client_secret = var.ovh_client_secret
 }
--- varibles.tf
+++ varibles.tf
@@ -1,3 +1,13 @@
+variable "ovh_client_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "ovh_client_secret" {
+  type      = string
+  sensitive = true
+}
+
 variable "service_name" {
   type        = string
   description = "ID du projet OVH Public Cloud (visible dans l'URL du manager : /public-cloud/pci/projects/<ID>)"
