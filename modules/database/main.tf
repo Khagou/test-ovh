8f8b49723311354c8181fb3c4706fb821c40fcb1
VERDOM <maxence.verdom@cgi.com>
2026-06-26 17:16:02 +0200

    need provider dans chaque module?

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    ovh = {
+      source  = "ovh/ovh"
+      version = "~> 0.46"
+    }
+  }
+}
+
 resource "ovh_cloud_project_database" "this" {
   service_name = var.service_name
   description  = var.description
--- modules/registry/main.tf
+++ modules/registry/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    ovh = {
+      source  = "ovh/ovh"
+      version = "~> 0.46"
+    }
+  }
+}
+
 resource "ovh_cloud_project_containerregistry" "this" {
   service_name = var.service_name
   name         = var.name
