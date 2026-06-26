420454b81cd2f0e1fdc02a1c138ecacaf855e475
VERDOM <maxence.verdom@cgi.com>
2026-06-26 11:24:38 +0200

    try plan OVH using service account

--- provider.tf
+++ provider.tf
@@ -5,11 +5,29 @@ terraform {
       version = "~> 0.46"
     }
   }
+
+  backend "s3" {
+    bucket = "test-tfstate"   # nom de ton bucket OVH Object Storage
+    key    = "ovh/terraform.tfstate"
+    region = "gra"       # adapte à ta région (gra, sbg, bhs…)
+
+    endpoints = {
+      s3 = "https://s3.eu-west-par.io.cloud.ovh.net/"  # endpoint S3 OVH de ta région
+    }
+
+    # Credentials S3 de l'OpenStack user créé pour le bucket
+    # Passés via env vars AWS_ACCESS_KEY_ID / AWS_SECRET_ACCESS_KEY
+    # (ou -backend-config à l'init)
+
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
+  }
 }
 
 provider "ovh" {
-  endpoint           = "ovh-eu"
-  application_key    = var.ovh_application_key
-  application_secret = var.ovh_application_secret
-  consumer_key       = var.ovh_consumer_key
+  endpoint      = "ovh-eu"
+  client_id     = "EU.12e8aed2e6667fcf"
+  client_secret = "064b3412188866a5ff2f58acefd48044Q"
 }
