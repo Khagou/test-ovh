37f6dae2193c1e4eba1066ee67626cece5d744eb
VERDOM <maxence.verdom@cgi.com>
2026-06-29 12:10:36 +0200

    ajout output password

--- outputs.tf
+++ outputs.tf
@@ -23,3 +23,6 @@ output "database_endpoints" {
 output "registry_urls" {
   value = { for k, v in module.registry : k => v.url }
 }
+output "passwork" {
+  value = { for k, v in module.registry : k => v.password }
+}
