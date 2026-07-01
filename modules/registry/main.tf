4f491b05607a2d048522299ca8fb59a1f273bc0c
VERDOM <maxence.verdom@cgi.com>
2026-07-01 11:30:58 +0200

    check terraform version

--- modules/registry/main.tf
+++ modules/registry/main.tf
@@ -8,6 +8,7 @@ resource "ovh_cloud_project_containerregistry" "registry" {
   plan_id      = data.ovh_cloud_project_capabilities_containerregistry_filter.regcap.id
   region       = data.ovh_cloud_project_capabilities_containerregistry_filter.regcap.region
   name         = var.registry_name
+
 }
 
 resource "ovh_cloud_project_containerregistry_user" "ci_user" {
